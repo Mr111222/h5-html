b17d71d2933bbf8653949da78d9b158b16277aba
zz <Mr_zhouzhi@163.com>
2018-08-21 14:57:41 +0800

    Update index.js
    修改  var  ev = e || window.event

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ $(function(){
 		var valPattern = /^((1[3,5,8,7][0-9])|(14[5,7])|(17[0,6,7,8])|(19[7]))\d{8}$/;
 		var pasPattern = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/;  //为数字字母的组合，不能全是数字，也不能全是字母
 		$("#inpVal").on('blur',function(e){ 
-			e.preventDefault()
+			var ev = e || window.event
+			ev.preventDefault()
 			let vals = $(this).val()
 			if(!valPattern.test(vals)){
 				alert('请输入正确的大陆手机号码')
+				return false
 			}
 			getUser()
 		})
 
 		$("#pasVal").on('blur',function(e){
-			e.preventDefault()
+			var  ev = e || window.event
+			ev.preventDefault()
 			let vals = $(this).val()
 			if(!pasPattern.test(vals)){
 				alert('密码仅接受8-16位字母和数字混合')
+				return false
 			}
 		})
 
